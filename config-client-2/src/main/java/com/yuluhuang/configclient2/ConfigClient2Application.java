41a7b7759eca7e29f7ad4d329f20f501c4712114
yuluhuang <504367857@qq.com>
2019-05-06 16:39:15 +0800

    config server

--- config-client-2/src/main/java/com/yuluhuang/configclient2/ConfigClient2Application.java
+++ config-client-2/src/main/java/com/yuluhuang/configclient2/ConfigClient2Application.java
@@ -1,19 +1,19 @@
-package com.yuluhuang.configclient;
+package com.yuluhuang.configclient2;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableEurekaClient
-public class ConfigClientApplication {
+@RefreshScope
+public class ConfigClient2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class, args);
+        SpringApplication.run(ConfigClient2Application.class, args);
     }
 
     @Value("${server.port}")
