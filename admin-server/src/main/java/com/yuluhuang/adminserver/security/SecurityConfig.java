8664cd2dd781018b32a5aa5e6e0d9e3be8876170
yuluhuang <504367857@qq.com>
2019-05-08 17:55:52 +0800

    admin

--- admin-server/src/main/java/com/yuluhuang/adminserver/security/SecurityConfig.java
+++ admin-server/src/main/java/com/yuluhuang/adminserver/security/SecurityConfig.java
@@ -9,12 +9,15 @@
  */
 package com.yuluhuang.adminserver.security;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author yoshikouamari
@@ -23,41 +26,51 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final String adminContextPath;
+
+    public SecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Page with login form is served as /login.html and does a POST on /login
-        http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-        // The UI does a POST on /logout on logout
-        http.logout().logoutUrl("/logout");
-        // The ui currently doesn't support csrf
-        http.csrf().disable();
+        // @formatter:off
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
 
-        // Requests for the login page and the static assets are allowed
         http.authorizeRequests()
-                .antMatchers("/login.html", "/**/*.css", "/assets/**", "/third-party/**")
-                .permitAll();
-        // ... and any other request needs to be authorized
-        http.authorizeRequests().antMatchers("/**").authenticated();
-
-        // Enable so that the clients can authenticate via HTTP basic for registering
-        http.httpBasic();
-//        super.configure(http);
-
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user")
-                .password("password")
-                .roles("USER")
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .withUser("admin")
-                .password("admin")
-                .roles("USER", "ADMIN");
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(
+                        adminContextPath + "/instances",
+                        adminContextPath + "/actuator/**"
+                );
+        // @formatter:on
+
+
     }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user")
+//                .password("password")
+//                .roles("USER")
+//                .and()
+//                .withUser("admin")
+//                .password("admin")
+//                .roles("USER", "ADMIN");
+//    }
 }
